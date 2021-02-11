1028e84d6faa2e8350ba40ef652a5901cbe240b4
Dorasima-98 <cjk1516@gmail.com>
2021-02-11 19:40:19 +0900

    server requests fcmserver sending messages to client (by token and by topic)

--- app/src/main/java/com/dorasima/fcmproject/MainActivity.java
+++ app/src/main/java/com/dorasima/fcmproject/MainActivity.java
@@ -11,12 +11,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("dorasima_topic");
 
         getToken();
     }
@@ -34,7 +40,34 @@ public class MainActivity extends AppCompatActivity {
                         // Get new FCM registration token
                         String token = task.getResult();
                         Log.d("test123", "token: "+ token);
+
+                        SendTokenThread thread = new SendTokenThread(token);
+                        thread.start();
                     }
                 });
+    }
+    // 서버로 토큰을 보내는 스레드
+    public class SendTokenThread extends Thread{
+        private String token;
+
+        SendTokenThread(String token){
+            this.token = token;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            try {
+                OkHttpClient client = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                builder = builder.url("http://192.168.35.240:8080/FCMProject/add_token.jsp?token="+token);
+
+                Request request =builder.build();
+                Call call = client.newCall(request);
+                call.execute();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
